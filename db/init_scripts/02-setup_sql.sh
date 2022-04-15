4f4e6e5a3d4923909b708b0b0714f04483898240
gaetanlefrioux <lefriouxgaetan@gmail.com>
2022-04-15 16:47:19 +0200

    Try to fix CRLF issue in bsh scripts

--- db/init_scripts/02-setup_sql.sh
+++ db/init_scripts/02-setup_sql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 export PGPASSWORD=$APP_DB_PASS;
+
 psql -v ON_ERROR_STOP=1 -U "$APP_DB_USER" -d "$APP_DB_NAME" -f /pg_setup/create_tables.sql
 psql -v ON_ERROR_STOP=1 -U "$APP_DB_USER" -d "$APP_DB_NAME" -f /pg_setup/func_aggregate_free_bikes.sql
 psql -v ON_ERROR_STOP=1 -U "$APP_DB_USER" -d "$APP_DB_NAME" -f /pg_setup/func_calculate_distance.sql
